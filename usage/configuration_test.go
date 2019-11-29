e6f36330d01c0859316fdc5cc2be8ce40385c54c
andrzej <dybowski@andrzejd.pl>
2019-11-29 09:37:14 +0100

    add test for new functional

---
+++ usage/configuration_test.go
@@ -0,0 +1,14 @@
+package usage
+
+import (
+	"github.com/andrzejd-pl/git-crawler/html"
+	"strings"
+	"testing"
+)
+
+func TestNewConfiguration(t *testing.T) {
+	got := ""
+	want :=
+
+	//configuration := NewConfiguraion(strings.NewReader(data))
+}
